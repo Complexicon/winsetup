526d1e4f2070629bb4e63936248d35101ecd8f90
Complexicon <contact@cmplx.tk>
2022-11-30 19:39:06 +0100

    Update setup-win.ps1

--- setup-win.ps1
+++ setup-win.ps1
@@ -1,20 +1,11 @@
 # test script
 
-function Test-Administrator  
-{  
-    [OutputType([bool])]
-    param()
-    process {
-        [Security.Principal.WindowsPrincipal]$user = [Security.Principal.WindowsIdentity]::GetCurrent();
-        return $user.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator);
-    }
-}
-
-if(-not (Test-Administrator))
-{
-    echo This script must be executed as Administrator
-    pause
-    exit 1;
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+ if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+  $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+  Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+  Exit
+ }
 }
 
 cd (gi $env:temp).fullname
